78596bccc6c349ab8303aecb83e2d52ce4b65cde
kcWu <79691025+kcwu229@users.noreply.github.com>
2022-01-17 02:21:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ function compute()
 {
     var p = document.getElementById("principal").value;
     var r = document.getElementById("rate").value;
-    var y = document.getElementById("years").value;
+    var y = parseInt(document.getElementById("years").value);
 
     var dateNow = new Date();
-    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(y);
+    var yearNow = parseInt(dateNow.getFullYear()) + y;
 
     var cal = p * r/100 * y;
 
-    document.getElementById("result").innerHTML = `If ypu deposit ${p}, 
+    if (parseInt(p) < 1 || p == "") {
+        alert("Please enter a positive number");
+    }
+    else {
+        document.getElementById("result").innerHTML = `If you deposit ${p}, 
                                                     \nat an interest rate of ${r}%.\n
                                                     You will receive an amount of ${cal},\n
                                                     in the year ${yearNow}`;
+    }
+
    
 }
         
