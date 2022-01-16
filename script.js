30d6b622afd8473a5c2f87362c315d91ab8afb0c
kcWu <79691025+kcwu229@users.noreply.github.com>
2022-01-16 22:56:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var cal = p * r/100 * y;
+
+    document.getElementById("result").innerHTML = `If ypu deposit ${p}, 
+                                                    \nat an interest rate of ${r}%.\n
+                                                    You will receive an amount of ${cal},\n
+                                                    in the year ${year + parseInt(y)}`;
+   
 }
         
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_val");
+
+    // 50%
+    output.innerHTML = slider.value; 
+
+    // Update value
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
